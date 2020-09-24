f40c9753413cd36d7735ae5506ede2b8d4313d10
junior <ijuniorfu@gmail.com>
2020-09-24 16:31:23 +0800

    custom

--- QuantumultX/Script/toutiao.js
+++ QuantumultX/Script/toutiao.js
@@ -33,8 +33,10 @@ function feed() {
     var obj = JSON.parse($response.body);
     if (obj.data) {
         for (var i = obj.data.length - 1; i >= 0; i--) {
-            if (obj.data[i].content.indexOf("raw_ad_data") > 0) {
-                obj.data.splice(i, 1);
+            if (typeof obj.data[i].content != 'undefined') {
+                if (obj.data[i].content.indexOf("raw_ad_data") > 0) {
+                    obj.data.splice(i, 1);
+                }
             }
         }
     }
